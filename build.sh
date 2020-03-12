112574b9d0235d0f8f51901d410e5c37fb2b6084
kbhatt <kamal.bhatt@stordis.com>
2020-03-12 10:01:10 +0100

    Removed master patch makes no sense as far as stability is concerned.

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #! /bin/sh
 git clone https://github.com/opencomputeproject/OpenNetworkLinux.git
 cd OpenNetworkLinux
-#Uncomment one of the commits to build
-#commit=ONLPv2
-#Best working version
+#Best working version of ONLPv2
 commit=d9730e34670429302377d277b429c3a0582ccdd4
 git checkout $commit
 patch -p2 < ../"$commit.patch"
